bc7d2ee3e0966febd084c25615ffbd2492229ec6
Dmitriy Vasin <vasin.mitya@gmail.com>
2021-04-16 17:59:48 +0300

    Выполнил задание №4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+   @FXML
+   TextArea mainTextArea;
+   @FXML
+   TextField mainTextField;
+   @FXML
+   Button sendButton;
+
+    public void sendMassage(){
+        if(mainTextField.equals("")) {
+            mainTextArea.appendText(mainTextField.getText() + "\n");
+            mainTextField.clear();
+            mainTextField.requestFocus();
+        }
+        else {
+            System.out.println("Введите текст сообщения");
+        }
+    }
 }
